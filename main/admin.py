bf5e5912daa01b429a4fb1d9d47d28093ecc5cd7
Greg Dingle <gdingle@chanzuckerberg.com>
2018-07-18 16:41:51 -0700

    add multi guides and primers support

--- main/admin.py
+++ main/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 
 from .models import *
+from .forms import *
 
 admin.site.register(Researcher)
 admin.site.register(Experiment)
-admin.site.register(GuideDesign)
 admin.site.register(GuideSelection)
 admin.site.register(GuidePlateLayout)
 admin.site.register(PrimerDesign)
 admin.site.register(PrimerSelection)
 admin.site.register(PrimerPlateLayout)
+
+
+@admin.register(GuideDesign)
+class GuideDesignAdmin(admin.ModelAdmin):
+    form = GuideDesignForm
