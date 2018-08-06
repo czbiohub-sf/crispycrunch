ebce97980d13d522513f15e1504fd090c0a2524a
Greg Dingle <gdingle@chanzuckerberg.com>
2018-08-06 15:55:54 -0700

    working

--- crispresso/app.py
+++ crispresso/app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+import os
+import subprocess
+
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,3 +9,45 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'HEllo world'
+
+
+def _get_trim_opt(adapterloc):
+    # adapterloc = args.input_dir + "/TruSeq3-PE-2.fa"
+    leadingqual = 3
+    trailingqual = 3
+    slidewindsiz = 4
+    slidewindqual = 20
+    minlength = 50
+
+    # TODO (gdingle): clean up
+    return 'ILLUMINACLIP:' + adapterloc + ':2:30:10 LEADING:' + str(leadingqual) \
+        + ' TRAILING:' + str(trailingqual) + ' SLIDINGWINDOW:' + str(slidewindsiz) + \
+        ':' + str(slidewindqual) + ' MINLEN:' + str(minlength)
+
+
+@app.route('/crispresso')
+def crispresso():
+    # TODO (gdingle): make it POST only
+    crispresso_cmd = [
+        '/opt/conda/bin/CRISPResso',
+        '--fastq_r1', request.args['fastq_r1'],
+        '--fastq_r2', request.args['fastq_r2'],
+        '--amplicon_seq', request.args['amplicon_seq'],
+        '--guide_seq', request.args['guide_seq'],
+        '--expected_hdr_amplicon_seq', request.args['expected_hdr_amplicon_seq'],
+        '-o', 'output',
+        '--save_also_png',
+        '--trim_sequences',
+        # TODO (gdingle): how to verify this is working?
+        '--trimmomatic_options_string',
+        "'" + _get_trim_opt(request.args['adapterloc']) + "'",
+    ]
+
+    # TODO (gdingle): why doesn't subprocess.check_output work here?
+    os.system(' '.join(crispresso_cmd))
+
+    return ' '.join(crispresso_cmd)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
