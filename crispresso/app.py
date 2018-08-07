94ad974764ae0820a6b154feb819848ea9c9a636
Greg Dingle <gdingle@chanzuckerberg.com>
2018-08-06 23:49:32 -0700

    execute script as function

--- crispresso/app.py
+++ crispresso/app.py
@@ -1,4 +1,5 @@
-import os
+import multiprocessing
+import sys
 
 from flask import Flask, request
 
@@ -8,6 +9,32 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'HEllo world'
+
+
+@app.route('/crispresso')
+def crispresso():
+    # TODO (gdingle): make it POST only
+    crispresso_args = [
+        '/opt/conda/bin/CRISPResso',
+        # TODO (gdingle): pull in fastq from other service, local file sys, or s3
+        '--fastq_r1', request.args['fastq_r1'],
+        '--fastq_r2', request.args['fastq_r2'],
+        '--amplicon_seq', request.args['amplicon_seq'],
+        '--guide_seq', request.args['guide_seq'],
+        '--expected_hdr_amplicon_seq', request.args['expected_hdr_amplicon_seq'],
+        '-o', 'output',
+        '--save_also_png',
+        '--trim_sequences',
+        # TODO (gdingle): what is fasta adapter?
+        '--trimmomatic_options_string', _get_trim_opt(request.args['adapterloc']),
+        '--n_processes', str(multiprocessing.cpu_count()),
+    ]
+
+    if not request.args.get('dryrun'):
+        _import_and_execute(crispresso_args)
+
+    # TODO (gdingle): return something useful
+    return ' '.join(crispresso_args)
 
 
 def _get_trim_opt(adapterloc):
@@ -24,32 +51,17 @@ def _get_trim_opt(adapterloc):
         ':' + str(slidewindqual) + ' MINLEN:' + str(minlength)
 
 
-@app.route('/crispresso')
-def crispresso():
-    # TODO (gdingle): make it POST only
-    crispresso_cmd = [
-        '/opt/conda/bin/CRISPResso',
-        # TODO (gdingle): pull in fastq from other service, local file sys, or s3
-        '--fastq_r1', request.args['fastq_r1'],
-        '--fastq_r2', request.args['fastq_r2'],
-        '--amplicon_seq', request.args['amplicon_seq'],
-        '--guide_seq', request.args['guide_seq'],
-        '--expected_hdr_amplicon_seq', request.args['expected_hdr_amplicon_seq'],
-        '-o', 'output',
-        '--save_also_png',
-        '--trim_sequences',
-        # TODO (gdingle): how to verify this is working?
-        '--trimmomatic_options_string',
-        "'" + _get_trim_opt(request.args['adapterloc']) + "'",
-    ]
-
-    # TODO (gdingle): why doesn't subprocess.check_output work here?
-    # TODO (gdingle): do we need shell=True? for PATH???
-    if not request.args.get('dryrun'):
-        # TODO (gdingle): return some useful status info
-        os.system(' '.join(crispresso_cmd))
-
-    return ' '.join(crispresso_cmd)
+def _import_and_execute(crispresso_args):
+    # HACK ALERT! Here we override sys.argv to run the CRISPResso script
+    # in the same process as the server, so we can better tracebacks
+    # and more python goodness.
+    sys.argv = crispresso_args
+    from CRISPResso.CRISPRessoCORE import main  # noqa
+    try:
+        main()
+    except SystemExit as e:
+        if e.code != 0:
+            raise e
 
 
 if __name__ == '__main__':
