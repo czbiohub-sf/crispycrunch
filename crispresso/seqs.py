1b531a1ad8708f739c6b7345b9bcec582d32e9d3
Greg Dingle <gdingle@chanzuckerberg.com>
2018-08-08 12:06:35 -0700

    add seqs.py 2

---
+++ crispresso/seqs.py
@@ -0,0 +1,11 @@
+import pysam
+
+
+def get_reference_amplicon(chr_loc):
+    """
+    >>> get_reference_amplicon('chr7:5569176-5569415')
+    """
+
+
+if __name__ == '__main__':
+    print('asdfdf')
