c95457a7b56cd23b220f74a6dd2d581c4b74d457
Greg Dingle <gdingle@chanzuckerberg.com>
2018-08-09 16:14:40 -0700

    samplesheet progress

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+# from django.test import TestCase
+# See https://stackoverflow.com/questions/2380527/django-doctests-in-views-py
 
-# Create your tests here.
+import doctest
+from main import samplesheet, platelayout, conversions, validators
+
+
+def load_tests(loader, tests, ignore):
+    tests.addTests(doctest.DocTestSuite(samplesheet))
+    tests.addTests(doctest.DocTestSuite(platelayout))
+    tests.addTests(doctest.DocTestSuite(conversions))
+    tests.addTests(doctest.DocTestSuite(validators))
+    return tests
