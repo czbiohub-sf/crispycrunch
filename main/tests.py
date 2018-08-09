e1d0b0b742028c6fb6ef2e19ed09e51bb3148006
Greg Dingle <gdingle@chanzuckerberg.com>
2018-08-09 16:40:10 -0700

    unit test

--- main/tests.py
+++ main/tests.py
@@ -1,13 +1,31 @@
-# from django.test import TestCase
-# See https://stackoverflow.com/questions/2380527/django-doctests-in-views-py
-
 import doctest
-from main import samplesheet, platelayout, conversions, validators
+
+from django.test import TestCase
+
+from main import conversions, platelayout, validators
+
+from main.models import *
+from main.samplesheet import *
 
 
 def load_tests(loader, tests, ignore):
-    tests.addTests(doctest.DocTestSuite(samplesheet))
+    """
+    Need to add doctests manually.
+    See https://stackoverflow.com/questions/2380527/django-doctests-in-views-py
+    """
     tests.addTests(doctest.DocTestSuite(platelayout))
     tests.addTests(doctest.DocTestSuite(conversions))
     tests.addTests(doctest.DocTestSuite(validators))
     return tests
+
+
+class SampleSheetTestCase(TestCase):
+
+    # TODO (gdingle): slim down the fixtures data
+    fixtures = ['main/tests.json']
+
+    def test_from_experiment(self):
+        # TODO (gdingle): pick a more interesting experiment
+        experiment = Experiment.objects.get(name='testsum3')
+        samplesheet = from_experiment(experiment)
+        self.assertTrue(len(samplesheet))
