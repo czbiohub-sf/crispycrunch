cebbf2b1814a64d326948d27bf79e8a88d88ef1e
Greg Dingle <gdingle@chanzuckerberg.com>
2018-08-10 18:15:33 -0700

    remove plate layout models

--- main/urls.py
+++ main/urls.py
@@ -16,17 +16,19 @@ urlpatterns = [
     # The parent object followed by the type of the object to-be-created.
     path(r'experiment/<int:id>/guide-design/', views.GuideDesignView.as_view(), name='Guide Design'),
     path(r'guide-design/<int:id>/guide-selection/', views.GuideSelectionView.as_view(), name='Guide Selection'),
-    path(r'guide-selection/<int:id>/guide-plate-layout/',
-         views.GuidePlateLayoutView.as_view(), name='Guide Plate Layout'),
+    # TODO (gdingle): do we still want this?
+    # path(r'guide-selection/<int:id>/guide-plate-layout/',
+    #      views.GuidePlateLayoutView.as_view(), name='Guide Plate Layout'),
 
     path(r'guide-selection/<int:id>/primer-design/', views.PrimerDesignView.as_view(), name='Primer Design'),
     path(r'primer-design/<int:id>/primer-selection/', views.PrimerSelectionView.as_view(), name='Primer Selection'),
-    path(r'primer-selection/<int:id>/primer-plate-layout/',
-         views.PrimerPlateLayoutView.as_view(), name='Primer Plate Layout'),
+    # TODO (gdingle): do we still want this?
+    # path(r'primer-selection/<int:id>/primer-plate-layout/',
+    #      views.PrimerPlateLayoutView.as_view(), name='Primer Plate Layout'),
     path(r'analysis/', views.AnalysisView.as_view(), name='Analysis'),
     # END EXPERIMENT CREATION URLS
 
-    path(r'primer-plate-layout/<int:id>/experiment-summary/',
+    path(r'primer-selection/<int:id>/experiment-summary/',
          views.ExperimentSummaryView.as_view(), name='Experiment Summary'),
     path(r'analysis/<int:id>/results/', views.ResultsView.as_view(), name='Experiment Analysis'),
     path(r'guide-plate-layout/<int:id>/order-form',
