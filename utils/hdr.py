8bbf919f9a53db12c3ef1ac927d8a7b4762bd574
Greg Dingle <gdingle@chanzuckerberg.com>
2018-10-10 14:40:56 -0700

    mutated_score

--- utils/hdr.py
+++ utils/hdr.py
@@ -1,25 +1,13 @@
 """
-Transforms genome sequences for HDR.
-
-target_seq contains codon
-target_seq contains insert loc
-target_seq contains guide_seq
-guide_seq contains pam
-guide_seq contains cut loc
-hdr_template contains hdr_seq
-mutated is copy of hdr_template
-mutated contains mutated guide_seq
-hdr_primer contains hdr_template
-
-codon_stop is reversed operation of codon_start
-
-# TODO (gdingle): MIT score on mutation
+Transformations of genome sequences for HDR.
 """
-
-# TODO (gdingle): how to subclass str?
 
 
 class HDR:
+    """
+    Encapsulates all the data and operations of a sequence for homolgous
+    directed repair.
+    """
 
     def __init__(
             self,
@@ -28,9 +16,9 @@ class HDR:
             hdr_tag: str = 'start_codon',
             hdr_dist: int = 0) -> None:
 
-        assert len(target_seq)
+        _validate_seq(target_seq)
         self.target_seq = target_seq
-        assert len(hdr_seq)
+        _validate_seq(hdr_seq)
         self.hdr_seq = hdr_seq
 
         assert hdr_tag in ('start_codon', 'stop_codon')
@@ -82,7 +70,7 @@ class HDR:
         >>> hdr._split_out_guide()
         ('', 'ATGGCTGAGCTGGAT', 'CCG', 'TTCGGC', '')
         """
-        assert len(self.target_seq) >= 24, 'Need to include guide plus PAM'
+        assert len(self.target_seq) >= 24, 'Need to include guide plus PAM plus one for codon'
         # align to codons
         last_codon = self.hdr_dist - (self.hdr_dist % 3)
         mutate_to = self.insert_at + last_codon
@@ -112,11 +100,9 @@ class HDR:
     @property
     def mutated(self) -> str:
         """
-        >>> hdr = HDR('ATGGCTGAGCTGGATCCGTTCGG', 'NNN', 'start_codon', 14)
-
-        # TODO (gdingle): make mutate_silently deterministic
-        >> hdr.mutated
-        'atggcagaacttgacCCgtacgt'
+        >>> hdr = HDR('ATGGCTGAGCTGGATCCGTTCGGC', 'NNN', 'start_codon', 14)
+        >>> hdr.mutated
+        'atggcggaactagacCCGtttgga'
         """
         before, guide_left, remainder, guide_right, after = self._split_out_guide()
         return ''.join((
@@ -126,6 +112,26 @@ class HDR:
             mutate_silently(guide_right),
             after,
         ))
+
+    @property
+    def mutated_score(self) -> float:
+        """
+        >>> hdr = HDR('ATGGCTGAGCTGGATCCGTTCGGC', 'NNN', 'start_codon', 14)
+        >>> hdr.mutated_score
+        7.775213510406977e-06
+        """
+        # TODO (gdingle): do for stop_codon
+        cut_at = self.insert_at + self.hdr_dist
+        # guide is shifted to include PAM because we mutate it
+        guide_right = cut_at + 6
+        guide_left = cut_at - 14
+
+        target_guide = self.target_seq[guide_left:guide_right]
+        assert target_guide.endswith('GG')
+
+        return mit_hit_score(
+            self.mutated[guide_left:guide_right],
+            target_guide)
 
 
 def mutate_silently(guide_seq: str) -> str:
@@ -145,7 +151,6 @@ def mutate_silently(guide_seq: str) -> str:
     synonymous = {
         # TODO (gdingle): comment out rare codons
         # see https://www.genscript.com/tools/codon-frequency-table
-        # TODO (gdingle): consider offtarget analysis of mutated
         'CYS': ['TGT', 'TGC'],
         'ASP': ['GAT', 'GAC'],
         'SER': ['TCT', 'TCG', 'TCA', 'TCC', 'AGC', 'AGT'],
@@ -177,7 +182,7 @@ def mutate_silently(guide_seq: str) -> str:
     new_guide = ''
     for i in range(0, len(guide_seq), 3):
         codon = guide_seq[i:i + 3].upper()
-        syns = set(synonymous[synonymous_index[codon]])
+        syns = list(synonymous[synonymous_index[codon]])
         if len(syns) > 1:
             syns.remove(codon)
         # TODO (gdingle): better to choose random syn?
@@ -189,6 +194,7 @@ def mutate_silently(guide_seq: str) -> str:
 
 def _validate_seq(seq: str):
     assert all(b.upper() in 'AGCTN' for b in seq)
+    assert len(seq)
 
 
 # TODO (gdingle): we may not need this at all because we can create from hdr_template above
@@ -233,6 +239,64 @@ def _validate_seq(seq: str):
 #             primer_product[-3:]
 
 #     assert False
+
+
+def mit_hit_score(seq1: str, seq2: str) -> float:
+    """Compute MIT mismatch score between two 20-mers
+
+    See 'Scores of single hits' on http://crispr.mit.edu/about
+    See calcHitScore in
+    https://github.com/maximilianh/crisporWebsite/blob/master/crispor.py
+
+    Parameters
+    ----------
+    seq1, seq2 : sequence
+        two 20-mers to compare
+
+    Returns
+    -------
+    float
+        MIT mismatch score between the two sequences
+
+    >>> mit_hit_score('AAGGCCAACCGGCGCCGCGC', 'GCGCGGCGCCGGTTGGCCTT')
+    6.039504885480631e-06
+    >>> mit_hit_score('GAAGGCCAACCGGCGCCGCG', 'CGCGGCGCCGGTTGGCCTTC')
+    1.6703747039472636e-05
+    """
+    # aka Matrix "M"
+    hit_score_m = [0, 0, 0.014, 0, 0, 0.395, 0.317, 0, 0.389, 0.079, 0.445, 0.508,
+                   0.613, 0.851, 0.732, 0.828, 0.615, 0.804, 0.685, 0.583]
+
+    assert(len(seq1) == 20)
+    max_dist = 19
+
+    assert(len(seq1) == len(seq2)), (seq1, seq2)
+
+    dists = []  # distances between mismatches, for part 2
+    mm_count = 0  # number of mismatches, for part 3
+    last_mm_pos = None  # position of last mismatch, used to calculate distance
+
+    score1 = 1.0
+    for pos in range(0, len(seq1)):
+        if seq1[pos] != seq2[pos]:
+            mm_count += 1
+            if last_mm_pos != None:
+                dists.append(pos - last_mm_pos)  # type: ignore
+            score1 *= 1 - hit_score_m[pos]
+            last_mm_pos = pos
+    # 2nd part of the score
+    if mm_count < 2:  # special case, not shown in the paper
+        score2 = 1.0
+    else:
+        avg_dist = sum(dists) / len(dists)
+        score2 = 1.0 / (((max_dist - avg_dist) / float(max_dist)) * 4 + 1)
+    # 3rd part of the score
+    if mm_count == 0:  # special case, not shown in the paper
+        score3 = 1.0
+    else:
+        score3 = 1.0 / (mm_count**2)
+
+    return score1 * score2 * score3 * 100
 
 
 if __name__ == '__main__':
