04faf03d630a90388359388f066db96412ed5342
Greg Dingle <gdingle@chanzuckerberg.com>
2018-11-05 16:32:27 -0800

    clean up

--- main/models.py
+++ main/models.py
@@ -24,7 +24,7 @@ from utils.validators import *
 
 VAR_TERMINUS_EXAMPLES = [
     # ENST00000447866,C length of cds only 36',
-    # ENST00000617316,N # Not Found for url: http://togows.org/search/ncbi-gene/ENST00000617316/1,50.json',
+    'ENST00000617316,N',
     'ENST00000278840,C',
     'ENST00000638572,N',
     'ENST00000361781,C',
