d85602b5d59c7b2a4c8faa77543f0d380cbb21e6
Greg Dingle <gdingle@chanzuckerberg.com>
2018-11-21 10:46:36 -0800

    add profiler

--- crispycrunch/settings.py
+++ crispycrunch/settings.py
@@ -273,4 +273,21 @@ INTERNAL_IPS = [
     '64.71.0.146',
 ]
 
+# See https://django-debug-toolbar.readthedocs.io/en/latest/panels.html#non-default-built-in-panels
+DEBUG_TOOLBAR_PANELS = [
+    # 'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    # 'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    # 'debug_toolbar.panels.templates.TemplatesPanel',
+    # 'debug_toolbar.panels.cache.CachePanel',
+    # 'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    # 'debug_toolbar.panels.profiling.ProfilingPanel',
+]
+
+
 # END ADDED BY GDINGLE
