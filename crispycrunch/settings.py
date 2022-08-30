4ebf86f14b5bdc8d0616754812d43cc352279916
Keith Cheveralls <keith.chev@gmail.com>
2022-08-30 16:57:56 -0700

    remove demo links from the home page and attempt to fix path to static assets

--- crispycrunch/settings.py
+++ crispycrunch/settings.py
@@ -252,7 +252,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'main', 'static')
 STATIC_URL = '/static/'
 
 # BEGIN ADDED BY GDINGLE
